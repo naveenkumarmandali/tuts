ac10b2c4b5fd43f787a4219e1fd2fadaa3f566e4
naveen <naveen.mandali7@gmail.com>
2024-01-10 14:56:14 +0530

    demo2 .java modified

--- Demo2.java
+++ Demo2.java
@@ -1,5 +1,6 @@
 class Demo2{
 	public static void main(String args[]){
-		System.out.println("Demo2 creating");
+		int a=10, int b=20
+		System.out.println("commiting Demo2.java modified="+"a+b="+ a+b);
 	}
 }
